a2bbf4cfb5ff1ed754a29fe21615ef623e1c6cf9
DirectorC <calvomaks@gmail.com>
2018-06-07 10:40:54 +0300

    metrics included+model persistence
    Added metrics for analysis and pickled the models for persistence

--- regression_stuff.py
+++ regression_stuff.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pickle
 from sklearn import preprocessing,linear_model
 import sklearn.metrics as sm
 import sys
 import matplotlib.pyplot as plt
 
+output_model_file='saved_model.pkl'
 file="C:/Users/ADMIN/Documents/Github/Python-Machine-Learning-Cookbook/Chapter01/data_singlevar.txt"
 x=[]
 y=[]
@@ -29,6 +31,15 @@ linear_regressor.fit(x_training,y_training)
 y_train_pred    =linear_regressor.predict(x_training)
 
 y_test_pred     =linear_regressor.predict(x_test)
+
+mean_absolute   =round(sm.mean_absolute_error(y_test,y_test_pred),2)
+mean_squared    =round(sm.mean_squared_error(y_test,y_test_pred))
+
+print(f"Mean absolute {mean_absolute}")
+print(f"Mean squared error {mean_squared}")
+
+with open(output_model_file,'wb') as file:
+    pickle.dump(linear_regressor,file)
 # plt.figure()
 # plt.scatter(x_test,y_test,color='red')
 # plt.plot(x_test,y_test_pred,color='green')
