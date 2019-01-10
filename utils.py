d4cef5b79fd5a1831a9c810ce1cf021e3d8a5835
DirectorC <calvomaks@gmail.com>
2019-01-11 01:28:44 +0300

    Implemented the Coursera single-variable linear regression in python

--- utils.py
+++ utils.py
@@ -20,13 +20,38 @@ def plot_feature_importance(feature_importance,title,feature_names):
     plt.show()
 
 def cost_function(x,y,theta):
-    pass
+    """Helps Obtain the cost function for any linear regressor"""
+    example_count     = x.shape[0]
+    squared_error     = (hypothesis_function(theta,x)-y)**2
+    sum_squared_error = sum(squared_error)
+    cost_value        = sum_squared_error/(2*example_count)
+    return cost_value
 
 def hypothesis_function(theta,x):
-    pass
+    """Helps Obtain the hypothesis for any linear regressor"""
+    return x.dot(theta)
 
 def feature_normalization(x):
+    """Helps with noramlizing feature data"""
     pass
 
 def normal_equation(x,y):
     pass
+def gradient_descent(x,y,theta,num_of_iter,alpha):
+    """Helps Obtain the optimal value of weighted values by minimizing cost function for any linear regressor"""
+    example_count   = x.shape[0]
+    cost_values     = np.zeros([num_of_iter,1])
+    for k in range(num_of_iter):
+        for j in range(theta.size):
+            error_factor      = (x.dot(theta)-y)*np.array([x[:,j]]).transpose()
+            error_sum_factor  = sum(error_factor)*(alpha/example_count)
+            theta[j]          = theta[j] - error_sum_factor
+        cost_values[k] = cost_function(x,y,theta)
+    return (theta,cost_values) 
+            
+            
+        
+        
+        
+    
+    
